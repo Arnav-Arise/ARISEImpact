f49e4b2afe6861be260efa9d87d71498d7aae25a
VivekBits2210 <vivekgoa1996@yahoo.co.in>
2016-06-28 00:48:13 +0530

    Add files via upload

--- wsgi/todoapp.py
+++ wsgi/todoapp.py
@@ -1,8 +1,9 @@
+# SQLALCHEMY_DATABASE_URI = os.environ['OPENSHIFT_POSTGRESQL_DB_URL']
 from datetime import datetime
-from flask import Flask,session, request, flash, url_for, redirect, render_template, abort ,g
+from flask import Flask, session, request, flash, url_for, redirect, render_template, abort, g
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, unicode
-from flask_login import login_user , logout_user , current_user , login_required
+from flask_login import login_user, logout_user, current_user, login_required
 from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
@@ -16,24 +17,32 @@ login_manager.login_view = 'login'
 
 class User(db.Model):
     __tablename__ = "users"
-    id = db.Column('user_id',db.Integer , primary_key=True)
-    username = db.Column('username', db.String(20), unique=True , index=True)
-    password = db.Column('password' , db.String(250))
-    email = db.Column('email',db.String(50),unique=True , index=True)
-    registered_on = db.Column('registered_on' , db.DateTime)
-    todos = db.relationship('Todo' , backref='user',lazy='dynamic')
+    id = db.Column('user_id', db.Integer, primary_key=True)
+    username = db.Column('username', db.String(20), unique=True, index=True)
+    password = db.Column('password', db.String(250))
+    email = db.Column('email', db.String(50), unique=True, index=True)
+    registered_on = db.Column('registered_on', db.DateTime)
+    todos = db.relationship('Todo', backref='user', lazy='dynamic')
+    org = db.Column('org', db.String(250))
+    name = db.Column('name', db.String(250))
+    # Note to self -Understand the index property
+    db.create_all()
+    db.session.commit()
 
-    def __init__(self , username ,password , email):
+    def __init__(self, username, password, email, name, org):
         self.username = username
         self.set_password(password)
         self.email = email
+        self.name = name
+        self.org = org
         self.registered_on = datetime.utcnow()
+        self.ctr=0
 
-    def set_password(self , password):
+    def set_password(self, password):
         self.password = generate_password_hash(password)
 
-    def check_password(self , password):
-        return check_password_hash(self.password , password)
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
 
     def is_authenticated(self):
         return True
@@ -50,6 +59,7 @@ class User(db.Model):
     def __repr__(self):
         return '<User %r>' % (self.username)
 
+
 class Todo(db.Model):
     __tablename__ = 'todos'
     id = db.Column('todo_id', db.Integer, primary_key=True)
@@ -58,7 +68,8 @@ class Todo(db.Model):
     done = db.Column(db.Boolean)
     pub_date = db.Column(db.DateTime)
     user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'))
-
+    db.create_all()
+    db.session.commit()
     def __init__(self, title, text):
         self.title = title
         self.text = text
@@ -70,8 +81,8 @@ class Todo(db.Model):
 @login_required
 def index():
     return render_template('index.html',
-        todos=Todo.query.filter_by(user_id = g.user.id).order_by(Todo.pub_date.desc()).all()
-    )
+                           todos=Todo.query.filter_by(user_id=g.user.id).order_by(Todo.pub_date.desc()).all()
+                           )
 
 
 @app.route('/new', methods=['GET', 'POST'])
@@ -88,40 +99,43 @@ def new():
             db.session.add(todo)
             db.session.commit()
             flash('Todo item was successfully created')
+#            ctr += 1
             return redirect(url_for('index'))
     return render_template('new.html')
 
-@app.route('/todos/<int:todo_id>', methods = ['GET' , 'POST'])
+
+@app.route('/todos/<int:todo_id>', methods=['GET', 'POST'])
 @login_required
 def show_or_update(todo_id):
     todo_item = Todo.query.get(todo_id)
     if request.method == 'GET':
-        return render_template('view.html',todo=todo_item)
+        return render_template('view.html', todo=todo_item)
     if todo_item.user.id == g.user.id:
         todo_item.title = request.form['title']
-        todo_item.text  = request.form['text']
-        todo_item.done  = ('done.%d' % todo_id) in request.form
+        todo_item.text = request.form['text']
+        todo_item.done = ('done.%d' % todo_id) in request.form
         db.session.commit()
         return redirect(url_for('index'))
-    flash('You are not authorized to edit this todo item','error')
-    return redirect(url_for('show_or_update',todo_id=todo_id))
+    flash('You are not authorized to edit this todo item', 'error')
+    return redirect(url_for('show_or_update', todo_id=todo_id))
 
 
-@app.route('/register' , methods=['GET','POST'])
+@app.route('/register', methods=['GET', 'POST'])
 def register():
     if request.method == 'GET':
         return render_template('register.html')
-    user = User(request.form['username'] , request.form['password'],request.form['email'])
+    user = User(request.form['username'], request.form['password'], request.form['email'], request.form['name'], request.form['org'])
     db.session.add(user)
     db.session.commit()
     flash('User successfully registered')
     return redirect(url_for('login'))
 
-@app.route('/login',methods=['GET','POST'])
+
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'GET':
         return render_template('login.html')
-    
+
     username = request.form['username']
     password = request.form['password']
     remember_me = False
@@ -129,27 +143,31 @@ def login():
         remember_me = True
     registered_user = User.query.filter_by(username=username).first()
     if registered_user is None:
-        flash('Username is invalid' , 'error')
+        flash('Username is invalid', 'error')
         return redirect(url_for('login'))
     if not registered_user.check_password(password):
-        flash('Password is invalid','error')
+        flash('Password is invalid', 'error')
         return redirect(url_for('login'))
-    login_user(registered_user, remember = remember_me)
+    login_user(registered_user, remember=remember_me)
     flash('Logged in successfully')
     return redirect(request.args.get('next') or url_for('index'))
+
 
 @app.route('/logout')
 def logout():
     logout_user()
-    return redirect(url_for('index')) 
+    return redirect(url_for('index'))
+
 
 @login_manager.user_loader
 def load_user(id):
     return User.query.get(int(id))
 
+
 @app.before_request
 def before_request():
     g.user = current_user
+
 
 if __name__ == '__main__':
     app.run()
